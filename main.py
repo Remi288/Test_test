7883f6b8d4e19d3d2e4aa8ef12988e553eeb9ded
Remi288 <iamopeoluwaadewale_28@yahoo.com>
2020-08-05 13:42:07 -0700

    Added scripts

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 def solution(A, K):
     # if the size of k > len(A), rotate only necessary with
    # module of the division
-    
+     k = 3
+   rotations = K % len(A)
+   return A[-rotations:] + A[:-rotations]
+
+
+print(solution([1, 2, 3, 6], 3))
